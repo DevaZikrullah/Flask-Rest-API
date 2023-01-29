f7304e7a70bb4f6b8bb65942dc7b5688f1d6c769
DevaZikrullah <zdeva804@gmail.com>
2023-01-29 20:02:38 +0700

    refactor register & info account

--- api/Controller/authController.py
+++ api/Controller/authController.py
@@ -1,29 +1,38 @@
 from api import Blueprint,request,render_template
 from api.Repositories.userRepository import *
+from api.Domain.authDomain import *
 
 authentication = Blueprint('auth_route',__name__)
 
 class Authentication:
     @authentication.route('/register', methods =['GET', 'POST'])
     def register():
-        username = request.form['username']
-        password = request.form['password']
-        point = request.form['point']
-        data = {
-            'username':username,
-            'password':password,
-            'point':point
-        }
-        if not UserRepository.getByName(username):
-            UserRepository.post(data)
-            return 
-        else:
-            return 
-        
-            
-    @authentication.route('/index')
-    def index():
-        name = 'Rosalia'
-        # username1 = request.form['username1']
-    
-        return render_template('index.html', title='Welcome')
+        username = request.json['username']
+        password = request.json['password']
+        point = request.json['point']
+        try:
+            AuthDomain.createAccount(username,password,point)
+            return jsonify(
+            status = 200,
+            message = 'Succses Creataed Account',
+            )
+        except:
+            return jsonify(
+                status=500,
+                message = 'Cant create Account'
+            )
+
+    @authentication.route('/info', methods =['GET'])
+    @jwt_required
+    def info_account():
+        try:
+            AuthDomain.info_my_acc(get_jwt_identity())
+            return jsonify(
+            status = 200,
+            message = 'Succses load info account',
+            )
+        except:
+            return jsonify(
+                status=500,
+                message = 'Failed load info account'
+            )
