e14aa34dafa0b7c605a20d12a71b1f9154ace12f
DevaZikrullah <zdeva804@gmail.com>
2023-01-30 22:33:49 +0700

    add login api

--- api/Domain/authDomain.py
+++ api/Domain/authDomain.py
@@ -15,33 +15,17 @@ authRoute = Blueprint('auth_route',__name__)
 
 
 class AuthDomain:
-    @authRoute.route('/login',methods =['POST'])
-    def login():
-        data = request.json
-        user = UserRepository.getByName(data['name'])
+    def login(username,password):
+        user = UserRepository.getByName(username)
 
         if not user:
-            return jsonify(
-                message = "not found name",
-                status = 404
-            )
+           raise Exception('not found user')
 
-        if check_password_hash(user.password,data['password']):
-            token = create_access_token(identity=user.name)
-            return jsonify(
-                # token = jwt.decode(token,app.config['SECRET_KEY'], algorithms="HS256"),
-                bearer_token= token,
-                message = "Login Succsesfull",
-                status = 201
-            )
-            
-        return jsonify(
-            message = "wrong password",
-            status = 401
-        )
+        if check_password_hash(user.password,password):
+            return 'Sucsess Login'
+        else:
+            raise Exception('Wrong Password')
 
-    # @authRoute.route('/info',methods =['GET'])
-    # @jwt_required()
     def info_my_acc(username):
         user = UserRepository.getByName(username)
         if not user:
